09285ce6eac38317f50c9b3389d876d2c613e197
Moses Holmström <thykka@gmail.com>
2024-03-16 18:53:47 +0200

    Clear logic from index

--- api/index.ts
+++ api/index.ts
@@ -2,8 +2,5 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 import { kv } from '@vercel/kv';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
-  return res.json({
-    message: `Hello ${name}!`,
-  })
+  return res.json({})
 }
